f2a9bf73db593bff591c0286f8244dad527d0940
James <james.ibberson@elder-studios.co.uk>
2021-06-22 09:57:20 +0100

    Install Laravel Breeze

---
+++ tests/Feature/ListProductsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ListProductsTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function it_paginates_a_list_of_products()
+    {
+        
+    }
+}
