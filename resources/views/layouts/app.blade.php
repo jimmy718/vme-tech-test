30b33c42e13cafd3eadc2c73cb74f0f225be0db5
James <james.ibberson@elder-studios.co.uk>
2021-06-24 17:08:45 +0100

    Add mysql container

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,10 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div id="app" class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -28,7 +28,7 @@
             </header>
 
             <!-- Page Content -->
-            <main>
+            <main id="app">
                 {{ $slot }}
             </main>
         </div>
