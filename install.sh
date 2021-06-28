3a7161b13c34f86dcb964deca2518e8e2cc46f99
James <james.ibberson@elder-studios.co.uk>
2021-06-28 12:53:53 +0100

    Add missing steps to install script

--- install.sh
+++ install.sh
@@ -12,11 +12,23 @@ php artisan key:generate
 echo "Start Sail containers"
 ./vendor/bin/sail up -d
 
+echo "Installing composer dependencies"
+./vendor/bin/sail composer install
+
+echo "Installing JavaScript dependencies"
+./vendor/bin/sail npm install
+
+echo "Building JavaScript"
+./vendor/bin/sail npm run dev
+
 echo "Migrate database schema"
 ./vendor/bin/sail artisan migrate
 
 echo "Import legacy products data (this will take a few moments)"
 ./vendor/bin/sail artisan db:seed
+
+echo "Running tests to be sure everything works"
+./vendor/bin/sail test
 
 echo "Image handling requires an s3 (or compatible) bucket setting up, otherwise"
 echo "Application available at http://localhost:8000 register a user and away you go!"
